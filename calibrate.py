1d0496454cefbff8eb2d763bf1732b6dc9af4922
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-09 11:25:58 -0400

    Added vanilla monte carlo

--- calibrate.py
+++ calibrate.py
@@ -1,21 +1,22 @@
 from scipy.optimize import minimize
 import monte_carlo
+import const
 
-#alpha is speed of reversion, theta is long term vol, phi is vol of vol
-def loss_function( alpha_0, theta_0, phi_0 ):
+#alpha is rate of reversion, theta is long term vol, phi is vol of vol
+def loss_function( params ):
+    alpha_0, theta_0, phi_0 = params
     mse = 0
     num = 0
     for t in T:
-        for i in range(len(strikes)):
-            for j in range(len(strikes[i])):
+        for i in range(len(const.strikes)):
+            for j in range(len(const.strikes[i])):
                 k = strikes[i][j]
                 p = prices[i][j]
-                s_0 = 100
                 mse += (vanilla_monte_carlo( s_0, sigma_0, k, t ) - p) ** 2
                 num += 1
     return mse / num
 
 
-def calibrate_sv( ):
+def calibrate_sv():
     initial_params = [0, 0, 0]
     return( minimize( loss_function, initial_params, tol=1e-6 ) )
--- monte_carlo.py
+++ monte_carlo.py
@@ -1,4 +1,5 @@
-import const as con
+import const
+import math 
 import numpy as np
 import matplotlib.pyplot as plt
 # np.standard_normal(1)
@@ -6,62 +7,54 @@ import matplotlib.pyplot as plt
 #alpha is the mean reversion rate
 #theta is the long term vol avg
 #phi is vol of vol
-'''def mc(n_sim, n_step, alpha, theta, phi):
-    miu = con.r
+def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.PUT):
+    miu = const.r
     s_T = 0
     p_T = 0
     for i in range (n_sim):
-        s_t = con.s_0
-        v_t = con.atm_iv_1m
+        s_t = s_0
+        v_t = sigma_0
         rv = 0
-        s_max = con.s_0
-        s_min = con.s_0
-        drawdown = 0
-        d_t = 1 / n_step
+        d_t = T / n_step
         for j in range (n_step):
+            if (v_t < 0):
+                v_t = 0
             d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
+            d_w_t_1 = d_w_t_1[0]
             d_s_t = miu * s_t *d_t + (v_t**0.5) * s_t * d_w_t_1
-            s_t_1 = s_t
-            s_t += d_s_t[0]
-            R_t = np.log(s_t/s_t_1)
-            rv += (R_t)**2
-            print(s_t_1, s_t)
-            if(s_t >= s_max):
-                cur_drawdown = (s_max - s_min) / s_max
-                print(s_max, s_min)
-                print(cur_drawdown)
-                if (cur_drawdown > drawdown):
-                    drawdown = cur_drawdown
-                s_max = s_t
-                s_min = s_t
-            elif (s_t < s_min):
-                s_min = s_t
-
+            s_t += d_s_t
             d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
-            d_v_t = alpha * ( theta - v_t) * d_t + phi * (v_t**0.5) * d_w_t_2
+            d_w_t_2 = d_w_t_2[0]
+            #d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
+            d_w_t_3 = d_w_t_2
+            #print(d_w_t_1, d_w_t_2)
+            d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_w_t_3
             v_t += d_v_t
-            rv /= n_step
-            if (drawdown > 0.1):
-                p_T += (rv - (con.bs_vol+0.05)**2) * con.notional
-        
+        if option == const.PUT:
+            payoff = strike - s_t
+        elif option == const.CALL:
+            payoff = s_t - strike
+        if( payoff < 0 ):
+            payoff = 0
+        p_T += payoff
         s_T += s_t
     s_T /= n_sim
     p_T /= n_sim
+    s_T *= math.exp(-const.r)
+    p_T *= math.exp(-const.r)
     return s_T, p_T
 
 
-print(mc_stock(1, 10, 0.01, 0.1, 0.02))'''
-
-def mc_df(n_sim, n_step, alpha, theta, phi):
+def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T):
     s = np.zeros((n_sim, n_step))
     p = np.zeros((n_sim,1))
-    mu = con.r
+    mu = const.r
     for i in range (n_sim):
-        s[i][0] = con.s_0
-        v_t = con.atm_iv_1m
+        s[i][0] = s_0
+        v_t = sigma_0
         rv = 0
-        s_max = con.s_0
-        d_t = 1 / n_step
+        s_max = const.s_0
+        d_t = T / n_step
         for j in range (n_step-1):
             d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
             d_s_t = mu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_w_t_1
@@ -70,17 +63,15 @@ def mc_df(n_sim, n_step, alpha, theta, phi):
             rv += (R_t)**2
             if(s[i][j+1] >= s_max):
                 s_max = s[i][j+1]
-
             d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
             d_v_t = alpha * ( theta - v_t) * d_t + phi * (v_t**0.5) * d_w_t_2
             v_t += d_v_t
             rv /= n_step
-        
         drawdown = (s_max-s[i][-1])/s_max
         if (drawdown > 0.1):
-            p[i] = max((rv - (con.bs_vol+0.05)**2),0) * con.notional   
+            p[i] = max((rv - (const.bs_vol+0.05)**2),0) * const.notional   
     return s
 
-mc_s = mc_df(10, 4, 0.01, 0.1, 0.02)
+mc_s = mc_df(10, 4, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 1)
 plt.plot(mc_s.T)
 plt.show()
