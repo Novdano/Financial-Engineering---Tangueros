d057c242e13e98258f6bafb8d4f65c8231527d30
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-09 18:09:58 -0400

    Added parallel code in Monte Carlo Vanilla and Changed callibration to global optimization

--- calibrate.py
+++ calibrate.py
@@ -1,11 +1,11 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, basinhopping
 import monte_carlo as mc
 import const
 
 #alpha is rate of reversion, theta is long term vol, phi is vol of vol
 def loss_function( params ):
-    alpha, theta, phi, rho = params
+    alpha, theta, phi = params
     mse = 0
     num = 0
     for t in const.T:
@@ -13,16 +13,23 @@ def loss_function( params ):
             for j in range(len(const.strikes[i])):
                 k = const.strikes[i][j]
                 p = const.prices[i][j]
-                mc_s, mc_p = mc.mc_vanilla(10, 100, alpha, theta, phi, rho, const.s_0, 
+                n_sim = 1000
+                n_step = 100
+                mc_s, mc_p = mc.mc_vanilla(n_sim, n_step, alpha, theta, phi, -0.3, const.s_0, 
                             const.atm_iv_1m, k, t, const.CALL )
-                pred_p = np.mean(mc_p)
+                pred_p = np.mean(mc_p) * (1 + const.r/n_step)**(-n_step)
                 mse += (pred_p - p) ** 2
                 num += 1
     return mse / num
 
 
 def calibrate_sv():
-    initial_params = [1, 1, 1, -0.3]
-    return( minimize( loss_function, initial_params, tol=1e-6 ) )
+    initial_params = [1, 1, 1]
+    constraints = ( {'type': 'ineq', 'fun': lambda x: x[0]},
+                    {'type': 'ineq', 'fun': lambda x: x[1]},
+                    {'type': 'ineq', 'fun': lambda x: x[2]} )
+    minimizer_kwargs = {"method": "BFGS"}
+    bounds = ((0, None), (0, 1), (0, 1))
+    return( basinhopping(loss_function, initial_params, minimizer_kwargs=minimizer_kwargs, niter=200) )
 
 print(calibrate_sv())
--- monte_carlo.py
+++ monte_carlo.py
@@ -2,39 +2,56 @@ import const
 import math 
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing as mp
+
+
+mc_p = []
+mc_s = []
+def collect_results(result):
+    global mc_p
+    global mc_s
+    mc_s.append(result[0])
+    mc_p.append(result[1])
+
+
+def one_simulation(n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.PUT):
+    miu = const.r       
+    s = np.zeros(n_step)
+    s[0] = s_0
+    v_t = sigma_0
+    rv = 0
+    d_t = T / n_step
+    for j in range(n_step-1):
+        if (v_t < 0):
+            v_t = 0
+        d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
+        d_s_t = miu * s[j] *d_t + (v_t**0.5) * s[j] * d_w_t_1
+        s[j+1] = s[j]+d_s_t[0]
+        d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
+        #d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
+        d_w_t_3 = d_w_t_2
+        #print(d_w_t_1, d_w_t_2)
+        d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_w_t_3
+        v_t += d_v_t
+    if option == const.PUT:
+        payoff = strike - s[-1]
+    elif option == const.CALL:
+        payoff = s[-1] - strike
+    if( payoff < 0 ):
+        payoff = 0
+    return s, payoff  
 
 #alpha is the mean reversion rate
 #theta is the long term vol avg
 #phi is vol of vol
 def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.PUT):
-    miu = const.r
-    s = np.zeros((n_sim, n_step))
-    p = np.zeros((n_sim,1))
+    #initiate pool
+    pool = mp.Pool(mp.cpu_count())
     for i in range (n_sim):
-        s[i][0] = s_0
-        v_t = sigma_0
-        rv = 0
-        d_t = T / n_step
-        for j in range(n_step-1):
-            if (v_t < 0):
-                v_t = 0
-            d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
-            d_s_t = miu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_w_t_1
-            s[i][j+1] = s[i][j]+d_s_t[0]
-            d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
-            #d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
-            d_w_t_3 = d_w_t_2
-            #print(d_w_t_1, d_w_t_2)
-            d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_w_t_3
-            v_t += d_v_t
-        if option == const.PUT:
-            payoff = strike - s[i][-1]
-        elif option == const.CALL:
-            payoff = s[i][-1] - strike
-        if( payoff < 0 ):
-            payoff = 0
-        p[i] = payoff
-    return s, p
+        pool.apply_async(one_simulation, args=(n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option), callback=collect_results)
+    pool.close()
+    pool.join()
+    return mc_s, mc_p
 
 
 def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T):
@@ -75,6 +92,9 @@ def plot_mc(s):
     plt.show()
 
 #mc_s, mc_p = mc_df(10, 4, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 1)
-mc_s, mc_p = mc_df(10, 4, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 1)
+mc_s, mc_p = mc_vanilla(1000, 100, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 100, 1)
+m = np.array(mc_s)
+print(np.mean(m, axis=0))
+print(np.mean(m, axis=0)[-1] * (1 + const.r/100)**(-100))
 #plt.plot(mc_s.T)
 #plt.show()
