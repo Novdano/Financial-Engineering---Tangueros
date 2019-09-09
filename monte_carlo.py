3a8ee23e0c9ff085c38012e3d4a4daa299863f3e
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-09 11:44:55 -0400

    Checked mc discounted stock martingale

--- monte_carlo.py
+++ monte_carlo.py
@@ -9,40 +9,33 @@ import matplotlib.pyplot as plt
 #phi is vol of vol
 def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.PUT):
     miu = const.r
-    s_T = 0
-    p_T = 0
+    s = np.zeros((n_sim, n_step))
+    p = np.zeros((n_sim,1))
     for i in range (n_sim):
-        s_t = s_0
+        s[i][0] = s_0
         v_t = sigma_0
         rv = 0
         d_t = T / n_step
-        for j in range (n_step):
+        for j in range(n_step-1):
             if (v_t < 0):
                 v_t = 0
             d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
-            d_w_t_1 = d_w_t_1[0]
-            d_s_t = miu * s_t *d_t + (v_t**0.5) * s_t * d_w_t_1
-            s_t += d_s_t
+            d_s_t = miu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_w_t_1
+            s[i][j+1] = s[i][j]+d_s_t[0]
             d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
-            d_w_t_2 = d_w_t_2[0]
             #d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
             d_w_t_3 = d_w_t_2
             #print(d_w_t_1, d_w_t_2)
             d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_w_t_3
             v_t += d_v_t
         if option == const.PUT:
-            payoff = strike - s_t
+            payoff = strike - s[i][-1]
         elif option == const.CALL:
-            payoff = s_t - strike
+            payoff = s[i][-1] - strike
         if( payoff < 0 ):
             payoff = 0
-        p_T += payoff
-        s_T += s_t
-    s_T /= n_sim
-    p_T /= n_sim
-    s_T *= math.exp(-const.r)
-    p_T *= math.exp(-const.r)
-    return s_T, p_T
+        p[i] = payoff
+    return s, p
 
 
 def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T):
@@ -72,6 +65,7 @@ def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T):
             p[i] = max((rv - (const.bs_vol+0.05)**2),0) * const.notional   
     return s
 
-mc_s = mc_df(10, 4, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 1)
-plt.plot(mc_s.T)
-plt.show()
+#mc_s = mc_df(10, 4, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 1)
+mc_s = mc_vanilla(100, 10000, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 100, 1)
+#plt.plot(mc_s.T)
+#plt.show()
