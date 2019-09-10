0d18718ab9a82abb8417e2d34f8d4555867d6e9a
serenaissleeping <gaosy08@gmail.com>
2019-09-09 21:54:56 -0400

    Update greeks.py

--- greeks.py
+++ greeks.py
@@ -5,16 +5,23 @@ import numpy as np
 
 def delta(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
     n_step_left = math.floor((T-t) * n_step / T)
-    mc_s_1, mc_p_1 = mc.mc_df (n_sim, n_step, alpha, theta, phi, rho, s_t-0.5, sigma_t, T-t)
-    mc_s_2, mc_p_2 = mc.mc_df (n_sim, n_step, alpha, theta, phi, rho, s_t+0.5, sigma_t, T-t)
+    mc_s_1, mc_p_1 = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t-0.5, sigma_t, T-t)
+    mc_s_2, mc_p_2 = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t+0.5, sigma_t, T-t)
     r = const.r
     res = math.exp(-r*(T-t)) * np.sum(mc_p_1) - math.exp(-r*(T-t)) * np.sum(mc_p_2)
     return res
 
 def vega(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
     n_step_left = math.floor((T-t) * n_step / T)
-    mc_s_1, mc_p_1 = mc.mc_df (n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t*1.005, T-t)
-    mc_s_2, mc_p_2 = mc.mc_df (n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t*0.995, T-t)
+    mc_s_1, mc_p_1 = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t, sigma_t*1.005, T-t)
+    mc_s_2, mc_p_2 = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t, sigma_t*0.995, T-t)
     r = const.r
     res = math.exp(-r*(T-t)) * np.sum(mc_p_1) - math.exp(-r*(T-t)) * np.sum(mc_p_2)
     return res
+
+
+def gamma(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
+    n_step_left = math.floor((T-t) * n_step / T)
+    delta_1 = delta(n_sim, n_step_left, alpha, theta, phi, rho, s_t-0.5, sigma_t, t, T)
+    delta_2 = delta(n_sim, n_step_left, alpha, theta, phi, rho, s_t+0.5, sigma_t, t, T)
+    return delta_1 - delta_2
