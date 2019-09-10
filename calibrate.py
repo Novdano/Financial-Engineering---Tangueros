ccdd9eface684500ccf5672cc88978096228d157
serenaissleeping <gaosy08@gmail.com>
2019-09-09 21:10:08 -0400

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -5,7 +5,7 @@ import const
 
 #alpha is rate of reversion, theta is long term vol, phi is vol of vol
 def loss_function( params ):
-    alpha, theta, phi, rho = params
+    alpha, theta, phi = params
     mse = 0
     num = 0
     for t in const.T:
@@ -13,7 +13,7 @@ def loss_function( params ):
             for j in range(len(const.strikes[i])):
                 k = const.strikes[i][j]
                 p = const.prices[i][j]
-                mc_s, mc_p = mc.mc_vanilla(10, 100, alpha, theta, phi, rho, const.s_0, 
+                mc_s, mc_p = mc.mc_vanilla(10, 100, alpha, theta, phi, -0.3, const.s_0, 
                             const.atm_iv_1m, k, t, const.CALL )
                 pred_p = np.mean(mc_p)
                 mse += (pred_p - p) ** 2
@@ -21,8 +21,15 @@ def loss_function( params ):
     return mse / num
 
 
-def calibrate_sv():
-    initial_params = [1, 1, 1, -0.3]
-    return( minimize( loss_function, initial_params, tol=1e-6 ) )
+'''def calibrate_sv(initial_params):
+    constraints = ( {'type': 'ineq', 'fun': lambda x: x[0]},
+                    {'type': 'ineq', 'fun': lambda x: x[1]},
+                    {'type': 'ineq', 'fun': lambda x: x[2]} )
+    bounds = ((0, None), (0, 1), (0, 1))
+    return(minimize(loss_function, initial_params, method="Anneal",
+        tol=1e-6, bounds=bounds))'''
 
-print(calibrate_sv())
+print(calibrate_sv([1,1,1]))
+
+
+
