877d696e67ee85b7cf775276270f7217e791b578
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-10 10:03:39 -0400

    Callibrate smile and term structure separately

--- calibrate.py
+++ calibrate.py
@@ -3,6 +3,11 @@ from scipy.optimize import minimize, basinhopping
 import monte_carlo as mc
 import const
 
+set_alpha = 1
+set_theta = 1
+set_phi = 0.08
+set_rho = -0.3
+
 #alpha is rate of reversion, theta is long term vol, phi is vol of vol
 def loss_function( params ):
     alpha, theta, phi = params
@@ -26,16 +31,18 @@ def loss_function_term_structure( params ):
     alpha, theta = params
     mse = 0
     num = 0
+    global set_phi
+    global set_rho
     for i in range(len(const.T)):
         t = const.T[i]
         atm_k = const.strikes[i][2]
         atm_p = const.prices[i][2]
         n_sim = 1000
         n_step = 1000
-        mc_s, mc_p = mc.mc_vanilla(n_sim, n_step, alpha, theta, 0.05, -0.3, const.s_0, 
-                    const.atm_iv_1m, atm_k, atmt, const.CALL )
+        mc_s, mc_p = mc.mc_vanilla(n_sim, n_step, alpha, theta, set_phi, set_rho, const.s_0, 
+                    const.atm_iv_1m, atm_k, t, const.CALL )
         pred_p = np.mean(mc_p) * (1 + const.r/n_step)**(-n_step)
-        mse += (pred_p - p) ** 2
+        mse += (pred_p - atm_p) ** 2
         num += 1
     return mse / num
 
@@ -43,6 +50,23 @@ def loss_function_term_structure( params ):
 
 def loss_function_smile( params ):
     phi = params[0]
+    rho = params[1]
+    mse = 0
+    num = 0
+    global set_alpha
+    global set_theta
+    for i in range(len(const.T)):
+        t = const.T[i]
+        atm_k = const.strikes[i][2]
+        atm_p = const.prices[i][2]
+        n_sim = 1000
+        n_step = 1000
+        mc_s, mc_p = mc.mc_vanilla(n_sim, n_step, set_alpha, set_theta, phi, rho, const.s_0, 
+                    const.atm_iv_1m, atm_k, t, const.CALL )
+        pred_p = np.mean(mc_p) * (1 + const.r/n_step)**(-n_step)
+        mse += (pred_p - atm_p) ** 2
+        num += 1
+    return mse / num
 
 
 '''def calibrate_sv(initial_params):
@@ -63,5 +87,20 @@ def calibrate_sv(initial_params):
     return( basinhopping(loss_function, initial_params, minimizer_kwargs=minimizer_kwargs, niter=200) )
 
 
-print(calibrate_sv([1,1,1]))
+def main():
+    bound_term = ((0, None), (0, 1))
+    bound_smile = ((0,1), (-1,1))
+    global set_alpha, set_theta, set_phi, set_rho
+    #calibrate alpha and theta
+    ts_param = minimize( loss_function_term_structure, [set_alpha, set_theta], tol=1e-8, bounds=bound_term)
+    print(ts_param)
+    set_alpha, set_theta = ts_param.x
+    smile_param = minimize( loss_function_smile, [set_phi, set_rho], tol=1e-8, bounds=bound_smile)
+    print(smile_param)
+    set_phi, set_rho = smile_param.x
+    print( set_alpha, set_phi, set_theta, set_rho )
+
+main()
+
+#print(calibrate_sv([1,1,1]))
 
