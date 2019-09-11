3de0965a05524a672df96e29c94e458856d5cf9d
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-11 13:15:07 -0400

    Fixed vanilla

--- monte_carlo.py
+++ monte_carlo.py
@@ -16,7 +16,7 @@ def collect_results(result):
 #alpha is the mean reversion rate
 #theta is the long term vol avg
 #phi is vol of vol
-def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.PUT):
+def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, option=const.CALL):
     miu = const.r
     s = np.zeros((n_sim, n_step))
     p = np.zeros((n_sim,1))
@@ -28,14 +28,14 @@ def mc_vanilla(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, strike, T, o
         for j in range(n_step-1):
             if (v_t < 0):
                 v_t = 0
-            d_w_t_1 = np.random.normal(0, d_t**0.5, 1)
-            d_s_t = miu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_w_t_1
+            d_w_t_1 = np.random.normal(0, 1, 1)
+            d_s_t = miu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_t**0.5 *d_w_t_1
             s[i][j+1] = s[i][j]+d_s_t[0]
-            d_w_t_2 = np.random.normal(0, d_t**0.5, 1)
-            #d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
-            d_w_t_3 = d_w_t_2
+            d_w_t_2 = np.random.normal(0, 1, 1)
+            d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
+            #d_w_t_3 = d_w_t_2
             #print(d_w_t_1, d_w_t_2)
-            d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_w_t_3
+            d_v_t = alpha * ( theta - v_t ) * d_t + phi * (v_t**0.5) * d_t**0.5 * d_w_t_3
             v_t += d_v_t
         if option == const.PUT:
             payoff = strike - s[i][-1]
@@ -80,7 +80,7 @@ def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_
     return s,p
 
 def check_s_martingale(s):
-    discounted_s = (np.sum(s,axis=0)[-1] / len(s)) * math.exp(- const.r)
+    discounted_s = np.sum( mc_s[:,-1] )/len( mc_s[:,-1] ) * math.exp(-const.r)
     return discounted_s
 
 def plot_mc(s):
@@ -96,7 +96,8 @@ def p_var(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_
     return var_low, var_high, mean, sd, mean/sd
 
 #mc_s, mc_p = mc_df(100, 1000, 2, 0.08, 0.2, -0.5, const.s_0, const.atm_iv_1m, 1)
-#mc_s, mc_p = mc_vanilla(1000, 100, 0.01, 0.1, 0.02, -0.3, const.s_0, const.atm_iv_1m, 100, 1)
+mc_s, mc_p = mc_vanilla(1000, 100, 2, 0.08, 0.2, -0.5, const.s_0, const.atm_iv_1m, 100, 1)
+print(check_s_martingale(mc_s))
 #m = np.array(mc_s)
 #print(np.mean(m, axis=0))
 #print(np.mean(m, axis=0)[-1] * (1 + const.r/100)**(-100))
