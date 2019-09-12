60e0e4c3823f0df90f072f0e36c3a0783e46b3f7
Novdano <ndedeyus@andrew.cmu.edu>
2019-09-12 17:44:04 -0400

    Fixed greeks finite differencing

--- greeks.py
+++ greeks.py
@@ -6,12 +6,12 @@ import numpy as np
 #1 dollar shock to stock
 def delta(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
     n_step_left = math.floor((T-t) * n_step / T)
-    mc_s_up, mc_p_up = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t + 1, sigma_t, T-t)
-    mc_s_down, mc_p_down = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t - 1, sigma_t, T-t)
+    mc_s_up, mc_p_up = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t + 0.01, sigma_t, T-t)
+    mc_s_down, mc_p_down = mc.mc_df (n_sim, n_step_left, alpha, theta, phi, rho, s_t - 0.01, sigma_t, T-t)
     r = const.r
     res = math.exp(-r*(T-t)) * np.sum(mc_p_up) - math.exp(-r*(T-t)) * np.sum(mc_p_down)
     print(math.exp(-r*(T-t)) * np.sum(mc_p_up), math.exp(-r*(T-t)) * np.sum(mc_p_down))
-    return res/2
+    return res/(2 * 0.01)
 
 #1 vol point shock
 def vega(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
@@ -30,8 +30,8 @@ def gamma(n_sim, n_step, alpha, theta, phi, rho, s_t, sigma_t, t, T):
     delta_2 = delta(n_sim, n_step_left, alpha, theta, phi, rho, s_t+0.5, sigma_t, t, T)
     return delta_1 - delta_2
 
-N_SIM = 10000
-N_STEP = 1000
+N_SIM = 100
+N_STEP = 100
 ALPHA = 10.97858327
 THETA = 0.12214962
 PHI = 0.00001
