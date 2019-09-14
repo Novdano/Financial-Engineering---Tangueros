e32f277706d23c15661ccbc2e3001fc5d367da6d
serenaissleeping <gaosy08@gmail.com>
2019-09-13 20:40:03 -0400

    update

--- monte_carlo.py
+++ monte_carlo.py
@@ -3,6 +3,17 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import multiprocessing as mp
+import pandas
+
+N_SIM = 100
+N_STEP = 100
+ALPHA = 10.97858327
+THETA = 0.02514962
+PHI = 0.01362476
+RHO = -0.55156066
+S_0 = const.s_0
+SIGMA_0 = const.atm_iv_1m
+TIME = 1
 
 #alpha is the mean reversion rate
 #theta is the long term vol avg
@@ -47,6 +58,7 @@ def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_
         v_t = sigma_0**2
         rv = 0
         d_t = T / n_step
+        s_max = s_0
         for j in range (n_step-1):
             if (v_t < 0):
                 v_t = 0
@@ -65,18 +77,68 @@ def mc_df(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_
             d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
             d_v_t = alpha * ( theta - v_t) * d_t + phi * (v_t**0.5) * d_t**0.5 * d_w_t_3
             v_t += d_v_t
+        #print(s[i][-1], s_max)
         drawdown = (s_max-s[i][-1])/s_max
         #print(drawdown)
         if (drawdown > 0.1):
-            p[i] = max((math.sqrt(rv) - (const.bs_vol+0.05)),0) * const.notional   
+            p[i] = max((math.sqrt(rv) - (const.bs_vol+0.05)),0) * const.notional 
+    print(np.count_nonzero(p)/n_sim)  
     return s, p
 
+def mc_report(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_0):
+    s = np.zeros((n_sim, n_step))
+    p = np.zeros((n_sim,1))
+    mu = const.r
+    v_T = np.zeros((n_sim,1))
+    rv_T = np.zeros((n_sim,1))
+    for i in range (n_sim):
+        s[i][0] = s_0
+        v_t = sigma_0**2
+        rv = 0
+        d_t = T / n_step
+        s_max = s_0
+        for j in range (n_step-1):
+            if (v_t < 0):
+                v_t = 0
+            d_w_t_1 = np.random.normal(0, 1, 1)
+            #jump = np.random.normal(const.jump_m,const.jump_v,1)
+            #d_n_t = np.random.normal(0, 1, 1)
+            #d_s_t = mu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_t**0.5 * d_w_t_1 \
+                        #+ s[i][j]*(math.exp(jump)-1)*d_n_t*d_t**0.5
+            d_s_t = mu * s[i][j] *d_t + (v_t**0.5) * s[i][j] * d_t**0.5 * d_w_t_1
+            s[i][j+1] = s[i][j]+d_s_t[0]
+            R_t = np.log(s[i][j+1]/s[i][j])
+            rv += (R_t)**2
+            if(s[i][j+1] >= s_max):
+                s_max = s[i][j+1]
+            d_w_t_2 = np.random.normal(0, 1, 1)
+            d_w_t_3= rho * d_w_t_1 + (1-rho**2)**0.5 * d_w_t_2
+            d_v_t = alpha * ( theta - v_t) * d_t + phi * (v_t**0.5) * d_t**0.5 * d_w_t_3
+            v_t += d_v_t
+        v_T[i] = v_t
+        rv_T = rv
+        drawdown = (s_max-s[i][-1])/s_max
+        #print(drawdown)
+        if (drawdown > 0.1):
+            p[i] = max((math.sqrt(rv) - (const.bs_vol+0.05)),0) * const.notional 
+    s_T = s[:,-1]
+    print('simulated vol at T, annualized realized vol, stock at T')
+    print("mean: ",np.mean(v_T), np.mean(rv_T), np.mean(s_T))
+    print("sd: ",np.stx(v_T), np.std(rv_T), np.std(s_T))
+
+    return v_T, rv_T, s_T
+
 def check_s_martingale(s):
-    discounted_s = np.sum( mc_s[:,-1] )/len( mc_s[:,-1] ) * math.exp(-const.r)
+    discounted_s = np.sum(mc_s[:,-1] )/len( mc_s[:,-1] ) * math.exp(-const.r)
     return discounted_s
 
-def plot_mc(s):
-    plt.plot(mc_s.T)
+def plot_mc(p, filename):
+    plt.hist(p, 50,facecolor='g')
+    plt.ylabel('Frequency')
+    plt.xlabel("Payoff")
+    plt.grid(True)
+    plt.title("Histogram of Structure Payoff")
+    plt.savefig(filename)
     plt.show()
 
 #returns the 5% VaR and 95% VaR
@@ -92,17 +154,15 @@ def p_var(n_sim, n_step, alpha, theta, phi, rho, s_0, sigma_0, T, s_max=const.s_
 #There's 5% chance that client may lose $0.1529 per 1$
 #There's a 95% chance that the client may lose $0.07980 oper 1$
 
-ALPHA = 10.97858327
-THETA = 0.0214962
-PHI = 0.01362476
-RHO = -0.55156066
-mc_s, mc_p = mc_df(100, 100, ALPHA, THETA, PHI, RHO, const.s_0, const.atm_iv_1m, 1)
-#print(np.mean(mc_p) * math.exp(-const.r))
-#mc_s, mc_p = mc_vanilla(1000, 1000, 2, 0.08, 0.2, -0.5, const.s_0, const.atm_iv_1m, 100, 1)
-#print("Price: %.5f" %( np.mean(mc_p) * math.exp(-const.r)))
-#print(check_s_martingale(mc_s))
-#m = np.array(mc_s)
-#print(np.mean(m, axis=0))
-#print(np.mean(m, axis=0)[-1] * (1 + const.r/100)**(-100))
-#plt.plot(mc_s.T)
-#plt.show()
+
+print("running...")
+mc_s, mc_p = mc_df(10000, 10000, ALPHA, THETA, PHI, RHO, const.s_0, const.atm_iv_1m, 1)
+np.save("mc_s_10000_10000",mc_s)
+np.save("mc_p_10000_10000",mc_p)
+print("saved")
+
+
+
+
+
+
