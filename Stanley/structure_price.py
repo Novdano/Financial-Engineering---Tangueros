fe1f43710e53c300637e1490e837ff78ce938b2e
SerenaGao <gaosy98@gmail.com>
2019-10-08 13:44:36 -0400

    gamma

--- Stanley/structure_price.py
+++ Stanley/structure_price.py
@@ -5,11 +5,7 @@ import numpy as np
 
 def knock_in_caplet(num_paths, alphas, K, phi, dt, t, T1, F = const.F):
     payoffs = []
-    prices = []
     knock_in = 0
-    three_m_rate = []
-    one_y_rate = []
-    spread = []
     P = []
     for i in range(len(F)):
         if(i == 0):
@@ -52,37 +48,68 @@ def knock_in_caplet(num_paths, alphas, K, phi, dt, t, T1, F = const.F):
         three_m_bond_yield = -math.log(curr_bond_price[risk_free_index])/dt
         one_y_bond_yield = -math.log(curr_bond_price[risk_free_index+3])
         final_spread = one_y_bond_yield - three_m_bond_yield
-        three_m_rate.append(three_m_bond_yield)
-        one_y_rate.append(one_y_bond_yield)
-        spread.append(final_spread)
         r = -math.log(three_m_bond_price)/dt
         percentage_change = (final_spread - initial_spread)/initial_spread
         if (percentage_change < -0.5):
             knock_in += 1
-            price = max((r - K),0) * discount 
+            payoff = max((r - K),0) * discount 
         else:
-            price = 0
-        payoffs.append(price / discount)
-        prices.append(price)
-    #np.savetxt( "1000000path_structure_payoff.csv" ,payoffs )
-    np.savetxt( "1000000path_3m_yield.csv" ,three_m_rate )
-    np.savetxt( "1000000path_1y_yield.csv" ,one_y_rate )
-    np.savetxt( "1000000path_spread.csv" ,spread )
-    #print(knock_in/num_paths)
-    payoff_std = np.std(np.array(payoffs))
-    # print("payoffs standard deviation", payoff_std)
-    # print("price", np.mean(payoffs))
-    # print("5% quantile", np.quantile(np.array(payoffs), 0.05))
-    # print("95% quantile", np.quantile(np.array(payoffs), 0.95))
-    return np.mean(price), np.std(price), np.mean(three_m_rate), np.mean(one_y_rate), np.mean(spread),\
-            np.std(three_m_rate), np.std(one_y_rate), np.std(spread)
-
-
-
-
+            payoff = 0
+        payoffs.append(payoff)
+    print(knock_in/num_paths)
+    payoff_std = np.std(np.array(payoffs)/discount)
+    print("payoffs standard deviation", payoff_std)
+    print("price", np.mean(payoffs))
+    print("5% quantile", np.quantile(np.array(payoffs)/discount, 0.05))
+    print("95% quantile", np.quantile(np.array(payoffs)/discount, 0.95))
+    return np.mean(payoffs)
 
 #print(knock_in_caplet(1000000, const.alphas, 0.0609, const.phi, 0.25, 0, 1))
 
 #knock_in_caplet(num_paths, alphas, K, phi, dt, t, T1)
+def delta(num_paths, alphas, K, phi, dt, t, T1, F = const.F):
+    F_up = copy.deepcopy(F)
+    F_down = copy.deepcopy(F)
+    bp = 0.0001
+    F_up[4] += bp
+    F_down[4] -= bp
+    price_up = knock_in_caplet(num_paths, alphas, K, phi, dt, t, T1, F_up)
+    price_down = knock_in_caplet(num_paths, alphas, K, phi, dt, t, T1, F_down)
+    d = (price_up - price_down) / 2
+    print("delta", d)
+    return d
+
+#print(delta(1000000, const.alphas, 0.0609, const.phi, 0.25, 0, 1))
+
+
+def gamma(num_paths, alphas, K, phi, dt, t, T1, F = const.F):
+    F_up = copy.deepcopy(F)
+    F_down = copy.deepcopy(F)
+    bp = 0.0001
+    F_up[4] += bp
+    F_down[4] -= bp
+    d_up = delta(num_paths, alphas, K, phi, dt, t, T1, F_up)
+    d_down = delta(num_paths, alphas, K, phi, dt, t, T1, F_down)
+    g = (d_up - d_down) / 2
+    print("gamma", g)
+    return g
+    
+print(gamma(1000000, const.alphas, 0.0609, const.phi, 0.25, 0, 1))
+
+def alpha_shock(num_paths, alphas, K, phi, dt, t, T1, F = const.F):
+    price = []
+    for i in range (len(alphas)):
+        alphas_i = copy.deepcopy(alphas)
+        alphas_i[i] *= 1.01
+        price.append(knock_in_caplet(num_paths, alphas_i, K, phi, dt, t, T1, F))
+    return price
+
+
+#print(alpha_shock(1000000, const.alphas, 0.0609, const.phi, 0.25, 0, 1, F = const.F))
+#shock_phi_price = knock_in_caplet(1000000, const.alphas, 0.0609, const.phi * 1.01, 0.25, 0, 1)
+structure_price = 0.00096671872
+#print(shock_phi_price - structure_price)
+
+
 
 
